e59d55ed78dea9a2ebb6c86abf9482c4c29780bc
AnshoriAbdulloh <areknautics@gmail.com>
2026-01-06 08:14:34 +0700

    add logout routes and controller

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { register, login, me } from "../controllers/auth.controller.js";
+import { register, login, me, logout } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 // setiap route akan menjalankan fungsi dari controllers
 router.post("/register", register);
 router.post("/login", login);
+router.post(`/logout`, logout);
 router.get("/me", me);
 
 export default router;
